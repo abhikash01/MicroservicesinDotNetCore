951f3bf01b68e8ceec06285a503f7b246d5d586c
Abhishek P <abhikash01@gmail.com>
2023-10-29 18:57:55 +0530

    Discount GRPC

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 namespace Discount.Grpc
@@ -13,11 +15,14 @@ namespace Discount.Grpc
 
             // Add services to the container.
             builder.Services.AddGrpc();
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+            builder.Services.AddAutoMapper(typeof(Program));
 
             var app = builder.Build();
+            app.MigrateDatabase<Program>();
 
             // Configure the HTTP request pipeline.
-            app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<DiscountService>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
